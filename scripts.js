5fbc8a475d278ea6be641fd212ebb2b6e17e114c
Stephanie Lin <stephanie_lin@brown.edu>
2025-01-11 18:31:13 -0500

    added light/dark, changed theme

--- scripts.js
+++ scripts.js
@@ -1,4 +1,32 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted!');
+// document.getElementById('contactForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     alert('Form submitted!');
+// });
+
+
+
+const toggle_button = document.getElementById('theme-toggle');
+
+if (localStorage.getItem('#theme') === 'light') {
+    document.documentElement.setAttribute('data-theme', 'light');
+}
+else{
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+}
+
+// add event listener
+document.addEventListener('DOMContentLoaded', () => {
+    toggle_button.addEventListener('click', function() {
+        const curr_theme = document.documentElement.getAttribute('data-theme');
+
+    
+        if (curr_theme === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
 });
